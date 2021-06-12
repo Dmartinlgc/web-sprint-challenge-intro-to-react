02eedf1ac3e9f4b308bd7252f782d5296baacae3
unknown <logicndepth@gmail.com>
2021-06-11 17:01:01 -0700

    added more styles

--- src/Character.js
+++ src/Character.js
@@ -10,8 +10,13 @@ justify-content:space-between;
 font-family: 'Orbitron', sans-serif;
 text-shadow: brown 3px 3px;
 padding-top: 1%;
+padding-left:1%;
+padding-right:1%;
 padding-bottom:1%;
 box-shadow:black 3px;
+background-image: url('https://us.123rf.com/450wm/natalia0103/natalia01031508/natalia0103150800030/44239332-diepe-ruimte-achtergrond-met-sterren-.jpg?ver=6');
+margin-bottom: 60px;
+border-radius: 10px;
 `
 
 const Character = (props)=>{
